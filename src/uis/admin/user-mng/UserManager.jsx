8203ff78cfe583bb53a0b1dbbf9c71dd66678abb
Trần Nam Dương <130457006+tnduongwarp@users.noreply.github.com>
2023-12-15 14:16:21 +0700

    add filter by time

--- src/uis/admin/user-mng/UserManager.jsx
+++ src/uis/admin/user-mng/UserManager.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { SearchOutlined, LoadingOutlined, DeleteFilled } from '@ant-design/icons';
-import { Button, Table, message, Popconfirm } from 'antd';
+import { Button, Table, message, Popconfirm, Select } from 'antd';
 import Api from '../../../services/Api';
 import { format } from 'date-fns';
 import './style.css';
@@ -87,16 +87,72 @@ export default function UserManagement(){
         let newdata = data.filter(item => (item.username.includes(key)));
         setFilterData(newdata)
     }
+    const handleChange = (value) => {
+      console.log(`selected ${value}`);
+      setKey('');
+      switch(value){
+        case 0: setFilterData(data); break;
+        case 1: {
+          const threeDaysAgo = new Date();
+          threeDaysAgo.setDate(threeDaysAgo.getDate()-3);
+          threeDaysAgo.setHours(0,0,0,0);
+          let newdata = data.filter(item => {
+            if(new Date(item.registration_date).getTime() >= threeDaysAgo.getTime()) return true;
+            else return false
+          })
+          setFilterData(newdata);
+          break;
+        };
+        case 2 : {
+          const aWeekAgo = new Date();
+          aWeekAgo.setDate(aWeekAgo.getDate()-7);
+          aWeekAgo.setHours(0,0,0,0);
+          let newdata = data.filter(item => {
+            if(new Date(item.registration_date).getTime() >= aWeekAgo.getTime()) return true;
+            else return false
+          })
+          setFilterData(newdata);
+          break;
+        };
+        case 3: {
+          const aMonthAgo = new Date();
+          aMonthAgo.setDate(aMonthAgo.getDate()-30);
+          aMonthAgo.setHours(0,0,0,0);
+          let newdata = data.filter(item => {
+            if(new Date(item.registration_date).getTime() >= aMonthAgo.getTime()) return true;
+            else return false
+          })
+          setFilterData(newdata);
+          break;
+        }
+        default: setFilterData(data)
+      }
+    };
     return (
         <>
         {
             loading ? <LoadingOutlined /> :
             <div>
                 <div className='search'>
-                    <input type="text" placeholder='Search user...' value={key} onChange={(e) => setKey(e.target.value)}/>
-                    <Button type="primary" icon={<SearchOutlined />}  onClick={onclickButton}>
-                        Search
-                    </Button>
+                    <div>
+                      <input style={{padding: '3px'}} type="text" placeholder='Search user...' value={key} onChange={(e) => setKey(e.target.value)}/>
+                      <Button type="primary" icon={<SearchOutlined />}  onClick={onclickButton}>
+                          Search
+                      </Button>
+                    </div>
+                    <div>
+                    <Select
+                      defaultValue = 'All'
+                      style={{ width: 120 }}
+                      onChange={handleChange}
+                      options={[
+                        { value: 0, label: 'All' },
+                        { value: 1, label: '3 days ago' },
+                        { value: 2, label: 'This week' },
+                        { value: 3, label: 'This month'},
+                      ]}
+                    />
+                    </div>
                 </div>
                 <Table columns={columns} dataSource={filterData} pagination={{ pageSize: 6}} loading = {loading}/>
             </div>
